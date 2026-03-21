6255e63472317396267872989d95b2f41ac95c45
alunosg <alunosg.altodalapa@gmail.com>
2026-03-21 10:33:54 -0300

    Game 1.1

--- PedroOnishi/Assets/Scripts/PlayerController.cs
+++ PedroOnishi/Assets/Scripts/PlayerController.cs
@@ -9,27 +9,23 @@ public class PlayerController : MonoBehaviour
     public float speed = 10;
     public float jumpForce = 10;
     public LayerMask floorLayer;
+    public Animator anim;
     private Vector2 moveInput;
+    private bool grounded = false;
+    private bool locked = false;
+    
 
     public void MoveInput(InputAction.CallbackContext context)
     {
-       moveInput = context.ReadValue<Vector2>();
+        moveInput = context.ReadValue<Vector2>();
+        anim.SetBool("IsWalking",moveInput.x !=0);
+        transform.localScale = new(
+            moveInput.x > 0 ? 1 : moveInput.x < 0 ? -1 : transform.localScale.x, 1);
     }
 
     public void JumpInput(InputAction.CallbackContext context)
     {
-        if(context.started)
-        {
-            ExecuteJump();
-        }
-    }
-
-    private void ExecuteJump()
-    {
-        Vector2 pLeft = new Vector2(col.bounds.min.x, col.bounds.max.y);
-        Vector2 pRight = new Vector2(col.bounds.max.x, col.bounds.max.y);
-        if(Physics.Raycast(pLeft, Vector2.down, col.bounds.size.y * 1.1f, floorLayer) ||
-            Physics.Raycast(pRight, Vector2.down, col.bounds.size.y * 1.1f, floorLayer))
+        if(context.started && grounded)
         {
             rig.linearVelocity = new Vector2
             (
@@ -39,8 +35,27 @@ public class PlayerController : MonoBehaviour
         }
     }
 
+    private void DetectGround()
+    {
+        Vector2 pLeft = new Vector2(col.bounds.min.x, col.bounds.max.y);
+        Vector2 pRight = new Vector2(col.bounds.max.x, col.bounds.max.y);
+        if(Physics2D.Raycast(pLeft, Vector2.down, col.bounds.size.y * 1.1f, floorLayer) ||
+            Physics2D.Raycast(pRight, Vector2.down, col.bounds.size.y * 1.1f, floorLayer))
+        {
+            grounded = true;
+        }
+        else
+        {
+            grounded = false;
+        }
+
+        anim.SetBool("IsGrounded", grounded);
+    }
+
     private void FixedUpdate()
     {
+        DetectGround();
+
         rig.linearVelocity = new Vector2
         (
             speed * moveInput.x,

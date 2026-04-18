154fa40d3246cdf96b18d49ae363b3734b00c53a
alunosg <alunosg.altodalapa@gmail.com>
2026-04-18 10:31:48 -0300

    Menu

---
+++ PedroOnishi/Assets/Scripts/Menu.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    [SerializeField] private GameObject creditsPanel;
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void OpenCredits()
+    {
+        creditsPanel.SetActive(true);
+    }
+
+    public void CloseCredits()
+    {
+        creditsPanel.SetActive(false);
+    }
+}
